ee09c46a7ad059f498b2aaea534fa9931b69e4d6
lcarranco <lcarranco@gmail.com>
2018-06-05 13:33:16 -0500

    Get and set number of queries and locations

--- main.cpp
+++ main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
-    cout << "Hello World!" << endl;
+    int q = 0;
+    int x = 0;
+    int y = 0;
+    int z = 0;
+    vector<int> locations;
+
+    cin >> q;
+    // cout << q << endl;
+
+    for (int i = 0; i < q; ++i) {
+        cin >> x;
+        // cout << x << endl;
+        locations.push_back(x);
+        cin >> y;
+        // cout << y << endl;
+        locations.push_back(y);
+        cin >> z;
+        // cout << z << endl;
+        locations.push_back(z);
+    }
+
+    // for (int i = 0; i < locations.size(); ++i) {
+    //     cout << locations[i] << ' ';
+    // }
+    
+
     return 0;
 }
